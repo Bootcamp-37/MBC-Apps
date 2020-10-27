ddd0b031b1c63d0edf7556b940f4ffa823deaaea
iqwalakmr <iqwalakmr@users.noreply.github.com>
2020-10-27 13:21:27 +0700

    Repository

---
+++ mbc/src/main/java/bootcamp37/mbc/repositories/RequestRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bootcamp37.mbc.repositories;
+
+import bootcamp37.mbc.entities.Request;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Deo Lahara
+ */
+
+@Repository
+public interface RequestRepository extends JpaRepository<Request, String> {
+    
+}
